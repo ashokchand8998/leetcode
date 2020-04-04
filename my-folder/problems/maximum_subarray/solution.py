2eb4ca050de0d1e1592ddfaaf32202afae481a54
Ashok Chand <ashokchand8998@gmail.com>
2020-04-04 05:47:03 +0000

    Sync LeetCode submission - Maximum Subarray (python)

---
+++ my-folder/problems/maximum_subarray/solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max_sum = nums[0]
+        curr_sum = 0
+        for i in nums:
+            if curr_sum <= 0:
+                curr_sum = 0
+                
+            curr_sum += i
+            
+            if curr_sum >= max_sum:
+                max_sum = curr_sum
+        return max_sum
