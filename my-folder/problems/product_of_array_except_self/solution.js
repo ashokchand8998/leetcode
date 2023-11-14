a7bf32d568c4114497510c42dc9ba30e1242a90f
Ashok Chand <ashokchand8998@gmail.com>
2023-11-14 12:13:09 +0000

    Sync LeetCode submission - Product of Array Except Self (javascript)

--- my-folder/problems/product_of_array_except_self/solution.js
+++ my-folder/problems/product_of_array_except_self/solution.js
@@ -2,14 +2,23 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+
+ /*
+ use prefix and postfix products stored and then combined to get final result
+ */
 var productExceptSelf = function (nums) {
     let result = [];
-    for(let i=0; i<nums.length; i++) {
-        result.push(nums.reduce((acc, b, index) => {
-            return ((index != i) ? acc*b : acc)
-        },1));
+    let prefix = 1;
+    let postfix = 1;
+    let n = nums.length
+    for(let i=0; i<n; i++) {
+        result[i] = prefix;
+        prefix *= nums[i];
     }
-    // let result = nums.map((num) => num ? (total / num) : total)
-    // console.log("result", result)
+
+    for(let j=n-1; j>=0; j--) {
+        result[j] *= postfix;
+        postfix *= nums[j];
+    }
     return result
 };
