c88e51a31ed0f06c4fdc6a4eb3ff4b5bc2b743cb
Ashok Chand <ashokchand8998@gmail.com>
2023-11-23 04:42:02 +0000

    Sync LeetCode submission - Valid Sudoku (javascript)

--- my-folder/problems/valid_sudoku/solution.js
+++ my-folder/problems/valid_sudoku/solution.js
@@ -3,43 +3,29 @@
  * @return {boolean}
  */
 var isValidSudoku = function (board) {
-    let rows = [];
-    let cols = [];
-    let grids = [];
-    for (let count = 0; count < 3; count++) {
-        for (let countj = 0; countj < 3; countj++) {
-            let grid = []
-            for (let i = countj * 3; i < ((countj + 1) * 3); i++) {
-                for (let j = count * 3; j < ((count + 1) * 3); j++) {
-                    if (board[i][j] != ".") {
-                        rows[i] = rows[i] || [];
-                        rows[i].push(board[i][j])
-                        cols[j] = cols[j] || [];
-                        cols[j].push(board[i][j])
-                        grid.push(board[i][j])
+    let rows = (new Array(9)).fill().map(() => new Array(9));
+    let cols = (new Array(9)).fill().map(() => new Array(9));
+    let grids = (new Array(3)).fill().map(() => new Array(3));
+
+    for (let row = 0; row < 9; row++) {
+        for (let col = 0; col < 9; col++) {
+            if (board[row][col] !== ".") {
+                let gridR = Math.floor(row/3);
+                let gridC = Math.floor(col/3);
+                if ((rows[row] && rows[row][board[row][col]]) ||
+                    (cols[col] && cols[col][board[row][col]]) ||
+                    (grids[gridR] && grids[gridR][gridC] && grids[gridR][gridC][board[row][col]])
+                ) {
+                    return false;
+                } else {
+                    rows[row][board[row][col]] = 1;
+                    cols[col][board[row][col]] = 1;
+                    if(!grids[gridR][gridC]) {
+                        grids[gridR][gridC] = []
                     }
+                    grids[gridR][gridC][board[row][col]] = 1;
                 }
             }
-            if (grid.length) {
-                grids.push(grid);
-            }
-        }
-    }
-    rows = rows.filter(row => !!row)
-    cols = cols.filter(col => !!col)
-    for (let row of rows) {
-        if (new Set(row).size !== row.length) {
-            return false;
-        }
-    }
-    for (let col of cols) {
-        if (new Set(col).size !== col.length) {
-            return false;
-        }
-    }
-    for (let i = 0; i < grids.length; i++) {
-        if (new Set(grids[i]).size !== grids[i].length) {
-            return false;
         }
     }
     return true;
